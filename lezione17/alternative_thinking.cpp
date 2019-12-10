7634e9b5d4c07d8dd3021994770c975d30db30a2
Riccardo Zanotto <rikyzan@gmail.com>
2019-12-10 11:58:25 +0100

    Done lecture 17

--- lezione17/alternative_thinking.cpp
+++ lezione17/alternative_thinking.cpp
@@ -1,1 +1,36 @@
 // http://codeforces.com/problemset/problem/603/A?locale=en
+
+/*
+Greedy algorithm: flipping a substring increaes at most by 2 the number of changes.
+In particular the flips that increase the score are the ones where there are two consecutive equal digits.
+
+Running time is O(n); memory is O(1)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    char s[N];
+    char prev = 'a';
+    int changes = 0;
+    int flips = 0;
+    for (int i=0;i<N;i++) {
+        cin >> s[i];
+        if (s[i]!=prev) changes++;
+        else {
+            if (flips<2) {
+                flips++;
+                changes++;
+            }
+        }
+        prev = s[i];
+    }
+
+    cout << changes << endl;
+
+    return 0;
+}
--- lezione17/wilbur_array.cpp
+++ lezione17/wilbur_array.cpp
@@ -1,1 +1,28 @@
 // http://codeforces.com/problemset/problem/596/B?locale=en
+
+/*
+We use a greedy algorithm: we keep the array A in the state b_1, b_2, ..., b_i, b_i, ..., b_i.
+We keep track of the current b_i in the cur variable and count the required +1,-1 operations.
+
+Running time is O(n); memory is O(1)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N, x;
+    cin >> N;
+    int cur = 0;
+    long long int steps = 0;
+
+    for (int i=0;i<N;i++) {
+        cin >> x;
+        steps += abs(x-cur);
+        cur = x;
+    }
+    cout << steps << endl;
+
+    return 0;
+}
