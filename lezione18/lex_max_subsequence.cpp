8025ecdf3b2fd8261a6532f32dd363c15068cda0
Riccardo Zanotto <rikyzan@gmail.com>
2019-12-10 13:07:44 +0100

    Some greedy algs of 18

--- lezione18/lex_max_subsequence.cpp
+++ lezione18/lex_max_subsequence.cpp
@@ -1,1 +1,28 @@
 // http://codeforces.com/problemset/problem/196/A?locale=en
+
+/*
+Greedy algorithm as seen at lecture: get characters from the end in increasing order.
+
+Running time is O(n); memory is O(n)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s, res;
+    cin >> s;
+    reverse(s.begin(), s.end());
+    res = "";
+    char M = '0';
+    for (auto c: s) {
+        if (c>=M) res += c;
+        M = max(M,c);
+    }
+
+    reverse(res.begin(),res.end());
+    cout << res << endl;
+
+    return 0;
+}
--- lezione18/woodcutters.cpp
+++ lezione18/woodcutters.cpp
@@ -1,1 +1,42 @@
 // http://codeforces.com/contest/545/problem/C?locale=en
+
+/*
+Greedy algorithm as seen at lecture: if the tree can fall left, do that; then check if it can fall right and do that.
+
+Running time is O(n); memory is O(1)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int h[N], x[N+1];
+    for (int i=0;i<N;i++) {
+        cin >> x[i] >> h[i];
+    }
+    x[N] = x[N-1]+h[N-1]+10;
+    int m = x[0];
+    int res = 1;
+    for (int i=1;i<N;i++) {
+        if (x[i]-h[i]>m) {
+            m = x[i];
+            res++;
+            //cout << "left" << endl;
+        } else if (x[i]+h[i]<x[i+1]) {
+            m = x[i]+h[i];
+            res++;
+            //cout << "right" << endl;
+        } else {
+            m = x[i];
+            //cout << "NO" << endl;
+        }
+    }
+
+    cout << res << endl;
+
+
+    return 0;
+}
